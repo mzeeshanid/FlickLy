fdc7f23cb5ed89fb44deb6e1c34934a048ea1a9a
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2023-07-29 16:10:37 +0500

    Initial Commit

---
+++ FlickLy/FlickLyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickLyApp.swift
+//  FlickLy
+//
+//  Created by Ali Aslam on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlickLyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
